cceaefb510f78b2058578b6113ad34a6fafc4f3c
Ricky Chu <ChuR@cardiff.ac.uk>
2019-10-18 11:17:29 +0100

    Created function to add locations and added 1 location

--- map.py
+++ map.py
@@ -1,92 +1,20 @@
 from items import *
 
-location_bus = {
-    "name": "An idle bus outside the student union",
+rooms = {}
 
-    "description":
-    """ """,
+def add_room(id, name, description, connected_places, items):
+    items[id] = {"name":name, "description":description, "connected_places":connected_places, "items":items}
 
-    "connected_places": {"Victoria_Coach_Station": 120},
+add_room("coach",
 
-    "items": [item_tape],
+        name="Coach outside cardiff uni"
 
-    ""
-}
+        description = """You are in a run-down, dusty bus that looks like it could break down
+        at any moment. The drive is staring down at you with beady eyes awaiting you ticket. You're estatic to arrive at London to meet
+        our childhood friend, Alex.""",
 
-location_coach_station = {
-    "name": "Victoria Coach Station",
+        connected_places = {victoria_coach_station:120},
 
-    "description":
-    """You are leaning agains the door of the systems managers'
-room. Inside you notice Matt "MJ" John and Simon Jones. They
-ignore you. To the north is the reception.""",
+        items = ["wagon wheels", "tape"]
 
-    "exits":  {"north": "Reception"},
-
-    "items": []
-}
-
-room_tutor = {
-    "name": "your personal tutor's office",
-
-    "description":
-    """You are in your personal tutor's office. He intently
-stares at his huge monitor, ignoring you completely.
-On the desk you notice a cup of coffee and an empty
-pack of biscuits. The reception is to the west.""",
-
-    "exits": {"west": "Reception"},
-
-    "items": []
-}
-
-room_parking = {
-    "name": "the parking lot",
-
-    "description":
-    """You are standing in the Queen's Buildings parking lot.
-You can go south to the COMSC reception, or east to the
-general office.""",
-
-    "exits": {"east": "Office", "south": "Reception"},
-
-    "items": []
-}
-
-room_office = {
-    "name": "the general office",
-
-    "description":
-    """You are standing next to the cashier's till at
-30-36 Newport Road. The cashier looks at you with hope
-in their eyes. If you go west you can return to the
-Queen's Buildings.""",
-
-    "exits": {"west": "Parking"},
-
-    "items": [item_pen]
-}
-
-room_lecture = {
-    "name": "the lecture hall",
-
-    "description":
-    """You are now at the lecture hall, Kirill Sidorov is having a chat
-    with the technician to fix the audio issues. You grab a seat and listen
-    to the lecture.""",
-
-    "exits":{"south":"Reception"},
-
-    "items": []
-
- }
-
-
-rooms = {
-    "Reception": room_reception,
-    "Admins": room_admins,
-    "Tutor": room_tutor,
-    "Parking": room_parking,
-    "Office": room_office,
-    "Lecture Hall": room_lecture
-}
+        )
