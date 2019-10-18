48f51ace79212ae24adc8b981f097b86ffecaa34
Oskar <Rickaby-PrattO@cardiff.ac.uk>
2019-10-18 11:47:48 +0100

    Maps updated, yeet

--- map.py
+++ map.py
@@ -18,3 +18,39 @@ add_room("coach",
         items = ["wagon wheels", "tape"]
 
         )
+
+add_room("victoria_coach_station",
+
+         name="London Victoria coach station"
+
+         description = """You've arrived at London Victoria Coach Station and it's a gloomy 8am.
+         As you step out of the coach, and grab your luggage, you push through the cluster of people
+         in the coach station and head outside. You have agreed to meet Kirill at the top floor of the Shard.""",
+
+         connected_places = {wetherspoons:30, taxi:10, tube:20},
+
+         items = ["coffee", "luggage", "molotov", "crayons"]
+
+         )
+
+add_room("wetherspoons",
+
+         name = "Wetherspoons"
+
+         description = """You enter the pub famed for its affordable food and drinks. The pub provides
+         a classic English interior that makes you feel right at home. There are plenty of people around,
+         causing a ruckus for the staff in this busy day.""",
+
+         connected_places = {victoria_coach_station:30}
+
+         items = ["beer", "whiskey", "coffee", "egg", "pork"]
+
+         )
+
+add_room("taxi",
+
+         name = "taxi"
+
+         description = """You call for a Taxi, a cab driver stops for you."""
+
+         
