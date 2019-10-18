e4b8b4f0ff3eda92c5b28fb51ad431fad3d91497
Lewis Mullarkey <mullarkeyl@cf.ac.uk>
2019-10-18 02:23:05 +0100

    I added 50 items that are completely obscure

--- items.py
+++ items.py
@@ -1,219 +1,306 @@
-item_wallet = {
-    "id": "wallet",
-
-    "name": "a wallet",
-
-    "mass": 0.3,
-
-    "properties":["container","droppable","light"],
-
-    "description":
-    """ """
-}
-
-item_luggage = {
-    "id": "luggage",
-
-    "name": "luggage",
-
-    "mass": 2,
-
-    "description":
-    """ """
-}
-
-item_money = {
-    "id": "money",
-
-    "mass": 0.5,
-
-    "name": "money",
-
-    "description":
-    ""
-}
-
-item_ticket = {
-    "id": "ticket",
-
-    "name": "a coach ticket",
-
-    "mass": 0.1,
-
-    "description": " "
-}
-
-item_phone = {
-    "id": "phone",
-
-    "name": "a smartphone",
-
-    "mass": 0.2,
-
-    "description": " "
-}
-
-item_tape = {
-    "id": "tape",
-
-    "name": "duck tape",
-
-    "mass": 1,
-
-    "description": "You never know when this might come in handy."
-}
-
-item_salmon = {
-    "id":"salmon",
-
-    "name":"a fillet of salmon",
-
-    "mass": 0.5,
-
-    "description":"Sometimes, you just gotta treat yourself with some fish."
-}
-
-item_molotov = {
-    "id":"molotov",
-
-    "name":"a molotov",
-
-    "mass":0.7,
-
-    "description":"The solution to every problem is to create another problem."
-}
-
-item_steel_ball = {
-    "id":"steel ball",
-
-    "name":"a steel ball",
-
-    "mass":5,
-
-    "description": " "
-}
-
-item_plank = {
-    "id":"plank",
-
-    "name":"a wooden plank",
-
-    "mass":2.5,
-
-    "description": " "
-}
-
-item_pen = {
-    "id":"pen",
-
-    "name":"a pen",
-
-    "mass": 0.1,
-
-    "description": " "
-}
-
-item_paper = {
-    "id":"paper",
-
-    "name":"paper",
-
-    "mass":0.1,
-
-    "description": " "
-
-}
-
-item_helmet = {
-    "id":"helmet",
-
-    "name":"a helmet",
-
-    "mass": 0.5,
-
-    "description": " "
-}
-
-item_crayons = {
-    "id":"crayons",
-
-    "name":"a box of crayons",
-
-    "mass":0.2,
-
-    "description":" "
-}
-
-item_matches ={
-    "id":"matches",
-
-    "name":"a box of matches",
-
-    "mass":0.3,
-
-    "description": " "
-
-}
-
-item_beer = {
-    "id":"beer",
-
-    "name":"beer",
-
-    "mass":0.2,
-
-    "description":" "
-}
-
-item_cereal = {
-    "id":"cereal",
-
-    "name":"cereal",
-
-    "mass":0.4,
-
-    "description":" "
-
-}
-
-item_protective_gear = {
-    "id":"gear",
-
-    "name":"protective gear",
-
-    "mass":2,
-
-    "description":" "
-}
-
-item_pan = {
-    "id":"pan",
-
-    "name":"a frying pan",
-
-    "mass":1,
-
-    "description": " "
-}
-item_egg = {
-    "id":"egg",
-
-    "name":"an egg",
-
-    "mass":0.1,
-
-    "description":" "
-}
-item_pork = {
-
-    "id":"pork",
-    "name":"porkchops",
-    "mass":0.2,
-    "description":""
-}
-items = [item_wallet,item_money,item_luggage,item_ticket,item_phone,item_tape,
- item_salmon, item_molotov, item_steel_ball, item_plank, item_pen,
- item_paper, item_helmet, item_crayons, item_matches, item_beer, item_cereal,
- item_protective_gear, item_egg,item_pork]
-
+items = {}
+def add_item(id, name, mass, properties, description):
+    items[id] = {"name":name, "mass":mass, "properties":properties, "description":description}
+
+add_item("wallet",
+    name = "a wallet",
+    mass = 0.3,
+    properties = [ "container", "droppable", "light"],
+    description=" ")
+
+
+add_item("luggage",
+    name= "luggage",
+    mass =  2,
+    properties = [],
+    description = "")
+
+
+add_item("money",
+    name= "money",
+    mass = 0.5,
+    properties = [],
+    description ="")
+
+add_item("ticket",
+    name = "a coach ticket",
+    mass = 0.1,
+    properties = [],
+    description ="")
+
+add_item("phone",
+    name= "a smartphone",
+    mass= 0.2,
+    properties = [],
+    description="")
+
+add_item("tape",
+    name= "duck tape",
+    mass= 1,
+    properties = [],
+    description = "You never know when this might come in handy.")
+
+add_item("salmon",
+    name="a fillet of salmon",
+    mass= 0.5,
+    properties = [],
+    description ="Sometimes, you just gotta treat yourself with some fish.")
+
+add_item("molotov",
+    name ="a molotov",
+    mass =0.7,
+    properties = [],
+    description ="The solution to every problem is to create another problem.")
+
+add_item("steel ball",
+    name = "a steel ball",
+    mass = 5,
+    properties = [],
+    description= "")
+
+add_item("plank",
+    name="a wooden plank",
+    mass=2.5,
+    properties = [],
+    description= "")
+
+add_item("pen",
+    name="a pen",
+    mass= 0.1,
+    properties = [],
+    description = "")
+
+add_item("paper",
+    name="paper",
+    mass=0.1,
+    properties = [],
+    description= "")
+
+add_item("helmet",
+    name="a helmet",
+    mass= 0.5,
+    properties = [],
+    description= "")
+
+add_item("crayons",
+    name = "a box of crayons",
+    mass = 0.2,
+    properties = [],
+    description = "")
+
+add_item("matches",
+    name="a box of matches",
+    mass=0.3,
+    properties =[],
+    description= "")
+
+add_item("beer",
+    name="beer",
+    mass=0.2,
+    properties = [],
+    description= "")
+
+add_item("cereal",
+    name="cereal",
+    mass=0.4,
+    properties=[],
+    description= "")
+
+add_item("gear",
+    name="protective gear",
+    mass=2,
+    properties = [],
+    description="")
+
+add_item("pan",
+    name="a frying pan",
+    mass=1,
+    properties = [],
+    description= "")
+
+add_item("egg",
+    name="an egg",
+    mass=0.1,
+    properties = [],
+    description= "")
+
+add_item("pork",
+    name="porkchops",
+    mass=0.2,
+    properties = [],
+    description= "")
+
+add_item("skateboard",
+    name="skateboard",
+    mass=0.3,
+    properties=[],
+    description="")
+
+add_item("whiskey",
+    name="whiskey",
+    mass=0.2,
+    properties=[],
+    description="")
+
+add_item("coffee",
+    name="coffee",
+    mass=0.2,
+    properties=[],
+    description="")
+
+add_item("wagon wheels",
+    name="wagon wheels",
+    mass=0.1,
+    properties=[],
+    description="The best biscuits ever made")
+
+add_item("sock",
+    name="a sock",
+    mass=0.1,
+    properties=[],
+    description="Can't find the other one.")
+
+add_item("butter",
+    name="a block of butter",
+    mass=0.1,
+    properties=[],
+    description="")
+
+add_item("steering wheel",
+    name="a steering wheel",
+    mass=0.1,
+    properties=[],
+    description="")
+
+add_item("trainers",
+    name="light-up trainers",
+    mass=0.1,
+    properties=[],
+    description="Used to distract your opponents")
+
+add_item("laces",
+    name="shoe laces",
+    mass=0.1,
+    properties=[],
+    description="Can you tie your shoe laces, though?")
+
+add_item("ring",
+    name="gold ring",
+    mass=0.1,
+    properties=[],
+    description="Someone took it off, but it won't make it that much easier")
+
+add_item("gondola",
+    name="a Venetian gondola",
+    mass = 10,
+    properties =[],
+    description="How did this even get to London?")
+
+add_item("cornetto",
+    name="a cornetto",
+    mass=0.1,
+    properties=[],
+    description="How's that for a slice of fried gold?")
+
+add_item("hdmi",
+    name="an HDMI cable",
+    mass=0.1,
+    properties=[],
+    description="")
+
+add_item("jar",
+    name="a jar",
+    mass=0.1,
+    properties=[],
+    description="")
+
+add_item("racket",
+    name="a tennis racket",
+    mass=0.3,
+    properties=[],
+    description="")
+
+add_item("lego yoda",
+    name="a lego yoda",
+    mass=0.2,
+    properties=[],
+    description="Rumour has it that he got banned for running people over with a 2001 Honda Civic")
+
+add_item("keys",
+    name="keys to a 2001 Honda Civic",
+    mass=0.1,
+    properties=[],
+    description="It's got blood all over it.")
+
+add_item("Honda Civic",
+    name="a Honda Civic",
+    mass = 30,
+    properties=[],
+    description="It's missing a steering wheel. I wonder where that went?")
+
+add_item("Helicopter",
+    name="a helicopter",
+    mass = 100,
+    properties=[],
+    description="")
+
+add_item("rubiks cube",
+    name="a Rubik's cube",
+    mass=0.1,
+    properties=[],
+    description="")
+
+add_item("tissue box",
+    name="an empty tissue box",
+    mass=0.1,
+    properties=[],
+    description="")
+
+add_item("tin foil",
+    name="tin foil",
+    mass=0.2,
+    properties=[],
+    description="")
+
+add_item("gift card",
+    name="a $10 roblox gift card",
+    mass=0.1,
+    properties=[],
+    description="")
+
+add_item("teapot",
+    name="a teapot",
+    mass=0.5,
+    properties=[],
+    description="")
+
+add_item("lime",
+    name="a lime",
+    mass=0.1,
+    properties=[],
+    description="IT'S A LIME!")
+
+add_item("fan",
+    name="a broken fan",
+    mass=0.5,
+    properties=[],
+    description="The cat broke it.")
+
+add_item("weetabix",
+    name="Weetabix",
+    mass=0.2,
+    properties=[],
+    description="")
+
+add_item("cornflakes",
+    name="Corn Flakes",
+    mass=0.2,
+    properties=[],
+    description="")
+    
+add_item("milk",
+    name="expired milk",
+    mass=0.1,
+    properties=[],
+    description="")
 print(len(items))
