03882dda587821a5ab8bfe59ec4286a474f59d2f
DEVAANG MISRA <misrad2@cardiff.ac.uk>
2019-10-21 11:15:49 +0100

    some more changes

--- game.py
+++ game.py
@@ -162,9 +162,16 @@ def execute_drop(item_id, current_location, inventory):
 
 def remove_item_from_player(item, inventory):
     """This remove the selected item from the players inventory"""
+    inventory.remove(item)
 
 def check_requirements(item_needed, inventory):
     """This checks if the player has all the items needed in their inventory"""
+    for c in inventory:
+        if inventory(c) == item_needed:
+            return True
+        else:
+            return False
+
 
 def calculate_time(player_properties, inventory,connected_places, place):
     """This calculates how long it'll take for the player to perform an action"""
