d86c098c7d0ea53a152a9d2cc3e7ad563715c58d
Lewis Mullarkey <mullarkeyl@cf.ac.uk>
2019-10-22 03:06:43 +0100

    changes

--- player.py
+++ player.py
@@ -57,6 +57,3 @@ add_character("pikachu",
 """Maybe change it so that you can pick a name"""
 
 """Due to the way we've changed how we represent the player, in order to call current_location we use characters["player"][current_location]"""
-
-for character in characters:
-    characters[character]['name']
