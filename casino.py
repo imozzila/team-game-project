96ac21400e1c039dccab5026c964da98f7d7b95e
Lewis Mullarkey <mullarkeyl@cardiff.ac.uk>
2019-10-23 20:07:38 +0100

    fixed higher or lower game

--- casino.py
+++ casino.py
@@ -2,24 +2,21 @@ import random
 
 def high_lower(inventory):
     print("Welcome to higher and lower!")
-
+    print("Guess the number between 1 and 10")
     count = 0
     win = False
-    final_num = random.randint(0, 500)
-    while not win:
-        player_guess = input("Guess the number")
+    final_num = random.randint(0, 10)
+    while not win and count < 5:
+        player_guess = int(input("Guess the number\n"))
         if player_guess > final_num:
             print("Too high!")
-            count+=1
         elif player_guess < final_num:
             print("Too low!")
-            count+=1
-        elif count > 5:
-            print("You took more than 5 turns...")
-            print("You lost. Thousands of kids gather around in a circle around you can point their fingers at you laughing. You pathetic loser.")
-        else:
-            print("You Win!")
+        elif player_guess == final_num:
             print("You win a think python 2e book!")
-
             win = True
+        count += 1
+    if not win:
+        print("You took more than 5 turns...")
+        print("You lost. Thousands of kids gather around in a circle around you can point their fingers at you laughing. You pathetic loser.")
     return win
